c4bdbfc1748ae645a87169769fe0098903a2187a
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-01-19 20:43:33 +0100

    chore: working on the new version

--- components/home.vue
+++ components/home.vue
@@ -4,11 +4,11 @@
 <template>
 	<div id="hero" class="flex w-full h-[40rem] gap-8 flex items-center justify-center p-8 md:p-0">
 		<div class="flex flex-col items-center">
-			<div class="p-1 w-16 h-16 rounded-full bg-[#f3523d]/25 flex items-center justify-center border-2 border-[#f3523d]">
+			<div class="p-1 w-16 h-16 rounded-full bg-rose-500/25 flex items-center justify-center border-2 border-rose-500">
 				<img src="/logo.png">
 			</div>
-            <div class="text-4xl lg:text-6xl text-center font-bold text-pretty md:w-[42rem]">Simplify your Dart & Flutter experience</div>
-            <div class="my-4 lg:my-8 text-xl text-center md:w-[42rem] text-pretty">
+            <div class="text-4xl lg:text-6xl text-center font-bold text-pretty md:w-[44rem]">The toolbox for your Dart & Flutter experience</div>
+            <div class="my-4 lg:my-8 text-xl text-center md:w-[44rem] text-pretty">
                 Our mission is to provide you with the best tools and resources to develop your applications.
             </div>
             <a href="https://discord.gg/zydgnJ3ksJ" class="max-w-fit bg-gradient-to-r from-rose-500 to-rose-600 px-8 py-4 rounded-full flex items-center gap-4"><span class="vpi-social-discord" style="--icon: url('https://api.iconify.design/simple-icons/discord.svg');"></span> Join the community</a>
--- components/projects.vue
+++ components/projects.vue
@@ -8,60 +8,69 @@ const projects = [
         description: 'The modular server-side framework of your dreams',
         image: '/birds/serinus-logo.png',
         link: 'https://serinus.app',
-        color: '#ff9800'
+        color: '#ff9800',
+        shadowColor: 'rgba(255, 152, 0, 0.5)'
     },
     {
         name: 'Acanthis',
         description: 'Your best pal for validating data',
         image: '/birds/acanthis-logo.png',
         link: 'https://acanthis.serinus.app',
-        color: '#c52233'
+        color: '#c52233',
+        shadowColor: 'rgba(197, 34, 51, 0.5)'
     },
     {
         name: 'Frontier',
         description: 'Simple Strategy-based Authentication library',
         image: '/birds/frontier-logo.png',
         link: 'https://github.com/avesbox/frontier',
-        color: '#f3523d'
+        color: '#5E72A7',
+        shadowColor: 'rgba(94, 114, 167, 0.5)'
     },
-    // {
-    //     name: 'Loxia',
-    //     description: 'Type-safe ORM for your Dart projects',
-    //     image: '/birds/loxia-logo.png',
-    //     link: 'https://github.com/avesbox/loxia'
-    // }
-]
-
-const currentProject = ref(0)
-
-onMounted(() => {
-    for (const bird of projects) {
-        const img = document.getElementById(bird.name);
-        img.addEventListener('mouseenter', () => {
-            const feather = document.createElement('div');
-            const featherImg = document.createElement('img');
-            featherImg.src = `/birds/${bird.name.toLowerCase()}-feather.png`;
-            feather.classList.add('absolute', 'w-8', 'transition-all', 'feather');
-            featherImg.classList.add('feather-side', 'transition-all');
-            feather.appendChild(featherImg);
-            feather.style.top = '0px';
-            feather.style.left = Math.random() * img.offsetWidth + 'px';
-            img.appendChild(feather);
-            feather.addEventListener('animationend', () => {
-                feather.remove();
-            })
-        })
+    {
+        name: 'Secure Session',
+        description: 'Secure stateless cookie session',
+        image: '/logo.png',
+        link: 'https://github.com/avesbox/secure_session',
+        color: '#f3523d',
+        shadowColor: 'rgba(243, 82, 61, 0.5)'
+    },
+    {
+        name: 'Microdiff',
+        description: 'A zero dependency object and array comparison library',
+        image: '/logo.png',
+        link: 'https://github.com/avesbox/microdiff',
+        color: '#f3523d',
+        shadowColor: 'rgba(243, 82, 61, 0.5)'
+    },
+    {
+        name: 'Dart Socket.io',
+        description: 'Port of JS/Node library Socket.io',
+        image: '/logo.png',
+        link: 'https://github.com/avesbox/dart_socket_io',
+        color: '#f3523d',
+        shadowColor: 'rgba(243, 82, 61, 0.5)'
     }
-})
+]
 
 </script>
 
 <template>
-    <div class="mx-auto flex flex-col justify-center lg:w-[64rem] relative mb-8">
+    <div class="mx-auto flex flex-col justify-center lg:w-[64rem] mb-8 p-4">
         <h1 class="text-2xl font-bold">Projects</h1>
         <p class="text-lg mb-8">We maintain and develop our projects focusing on the developer experience and their interoperability to offer you the best tooling for Dart</p>
-        <div class="w-full grid grid-cols-2">
-            
+        <div class="w-full grid lg:grid-cols-2 gap-4">
+            <a :href="project.link" v-for="project of projects" :key="project.name" class="w-full p-4 rounded-lg relative transition-all grayscale hover:grayscale-0" :style="{'boxShadow': '0 0 4px ' + project.shadowColor}">
+                <div class="flex justify-between items-center">
+                    <div class="flex flex-col">
+                        <div class="text-xl text-zinc-900 font-semibold">{{ project.name }}</div>
+                        <span class="text-sm text-zinc-600 font-normal text-pretty">{{ project.description }}</span>
+                    </div>
+                    <div class="p-1 w-16 h-16 rounded-full flex items-center justify-center border-2" :style="{backgroundColor: project.shadowColor, borderColor: project.color}">
+                        <img :src="project.image">
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
 </template>
@@ -75,6 +84,9 @@ onMounted(() => {
 .vp-doc a {
     color: initial;
     text-decoration: none;
+    transition-property: all;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 300ms;
 }
 
 .bird:hover > img {
